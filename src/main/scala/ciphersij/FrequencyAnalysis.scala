348acb4fd500de79c6f418e235f9676d3e8be850
Ivan Jasjko <i.jasjko@elsevier.com>
2020-06-09 20:52:25 +0100

    Added working frequency analysis method

--- src/main/scala/ciphersij/FrequencyAnalysis.scala
+++ src/main/scala/ciphersij/FrequencyAnalysis.scala
@@ -8,26 +8,50 @@ object FrequencyAnalysis {
     val cipherTextFrequencies = getLetterFrequencies(cipherText)
     val sampleTextFrequencies = getLetterFrequencies(sampleText)
 
-    val decryptMapping = cipherTextFrequencies.zipWithIndex.map({case (letter, index) => (letter, sampleTextFrequencies(index))}).toMap
+    println(s"Ciphertext letter frequencies: $cipherTextFrequencies\n")
+    println(s"Sampletext letter frequencies: $sampleTextFrequencies\n")
 
-    cipherText
-      .toSeq
-      .map(character => replaceLetter(character, decryptMapping))
-      .mkString("")
+    val cipherToSampleMapping = cipherTextFrequencies.map(x => x._1)
+      .zipWithIndex
+      .map({ case (letter, index) =>
+        (letter, sampleTextFrequencies(index)._1)
+      }).toMap
+
+    println(s"Frequency based cipher to sample mapping: $cipherToSampleMapping\n")
+
+    val keyCandidates = cipherTextFrequencies.take(5)
+      .map({ case (cipherLetter, frequency) =>
+        (cipherLetter, frequency, Caesar.alphabetMap(cipherLetter) - Caesar.alphabetMap(cipherToSampleMapping(cipherLetter)))
+      })
+
+    println(s"Candidate keys based on distance between most frequent letter positions: $keyCandidates\n")
+
+    val keyNums = keyCandidates.groupBy { case (_, _, distance) => distance }
+      .map({ case (key, letters) => (key, letters.size) })
+
+    if (keyNums.maxBy(_._2)._2 > 1) {
+      decipher(cipherText, keyNums.maxBy(_._2)._1)
+    } else {
+      println("None of the keys have the same distance, attempting all of them:")
+      keyNums.keys.foreach { n =>
+        println(s"${decipher(cipherText, n)}")
+      }
+      decipher(cipherText, keyNums.keys.head)
+    }
   }
 
-  private def getLetterFrequencies(text: String): Seq[Char] = {
+  private def decipher(cipherText: String, keyNum: Int): String = {
+    if (keyNum > 0) {
+      Caesar.encrypt(cipherText, math.abs(keyNum), "left")
+    } else {
+      Caesar.encrypt(cipherText, math.abs(keyNum), "right")
+    }
+  }
+
+  private def getLetterFrequencies(text: String): Seq[(Char, Int)] = {
     text
-      .replaceAll("[0-9]| ", "")
+      .replaceAll("""[0-9]| """, "")
       .distinct.map(character => (character, text.count(_ == character)))
       .sortBy({ case (_, frequency) => -frequency })
-      .map(x => x._1)
-  }
-
-  private def replaceLetter(character: Char, mapping: Map[Char, Char]): Char = {
-    if (character.isLetter)
-      mapping(character)
-    else
-      character
   }
 }
--- src/main/scala/ciphersij/Main.scala
+++ src/main/scala/ciphersij/Main.scala
@@ -4,7 +4,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val plaintext = TxtReader.readFile("text.txt")
-    val ciphertext = Caesar.encrypt(plaintext, 13, "left")
+    val ciphertext = Caesar.encrypt(plaintext, 42, "right")
     val decrypted = FrequencyAnalysis.decrypt(ciphertext)
 
     println(s"Plaintext:\n$plaintext\n")
